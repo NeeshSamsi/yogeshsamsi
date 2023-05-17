cc4616df26c63375886aee57d6a0471d28b492cb
Neesh Samsi <avaneeshsamsi@gmail.com>
2023-05-17 21:12:28 +0530

    feat: Rename spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
@@ -21,10 +21,10 @@ module.exports = {
         lighter: "#F5E0CC",
       },
       spacing: {
-        "col-all": "10%",
+        "col-wide": "10%",
         "col-main": "20%",
       },
     },
   },
   plugins: [],
-}
+};
