4f1687d02dc12ae0a392d7ab74298cd7f0d056f7
Neesh Samsi <avaneeshsamsi@gmail.com>
2023-05-17 21:13:08 +0530

    feat: Add Nav to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,37 @@
-import "./globals.css"
-import { Montserrat } from "next/font/google"
-import localFont from "next/font/local"
-import * as config from "@/lib/config"
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
+import * as config from "@/lib/config";
+import Navbar from "@/components/Navbar";
 
-const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "500", "600"], variable: "--font-montserrat" })
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  variable: "--font-montserrat",
+});
 
 const reckless = localFont({
   src: [
-    { path: "../fonts/RecklessNeue/ttf/RecklessNeue-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../fonts/RecklessNeue/ttf/RecklessNeue-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../fonts/RecklessNeue/ttf/RecklessNeue-Bold.ttf", weight: "700", style: "normal" },
+    {
+      path: "../fonts/RecklessNeue/ttf/RecklessNeue-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/RecklessNeue/ttf/RecklessNeue-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/RecklessNeue/ttf/RecklessNeue-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
   ],
   variable: "--font-reckless",
-})
+});
 
-const { title, description } = config.meta.home
+const { title, description } = config.meta.home;
 
 export const metadata = {
   metadataBase: new URL(config.url),
@@ -39,14 +56,22 @@ export const metadata = {
   themeColor: "#362009",
   alternates: {
     canonical: "/",
-    
   },
-}
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable} ${reckless.variable} font-sans`}>{children}</body>
+      <body
+        className={`${montserrat.variable} ${reckless.variable} bg-lighter font-sans text-darker`}
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
