e54a207a0afb7d53856e9be60872c7b11a0b5a1e
Neesh Samsi <avaneeshsamsi@gmail.com>
2023-05-18 13:23:04 +0530

    feat: Add dynamic hover colors

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -1,17 +1,22 @@
+import type { FC } from "react"
 import * as config from "@/lib/config"
 import Link from "next/link"
 import Facebook from "./Facebook"
 import Instagram from "./Instagram"
 import YouTube from "./YouTube"
 
-const index = () => {
+type Props = {
+  hoverClr: string
+}
+
+const index: FC<Props> = ({ hoverClr }) => {
   return (
     <>
       {config.socials.map(({ platform, link }, i) => (
         <Link
           href={link}
           key={i}
-          className="h-8 w-8 transition-colors hover:text-light md:h-6 md:w-6 lg:h-7 lg:w-7"
+          className={`h-8 w-8 transition-colors md:h-6 md:w-6 lg:h-7 lg:w-7 hover:${hoverClr}`}
         >
           <li>
             {(() => {
