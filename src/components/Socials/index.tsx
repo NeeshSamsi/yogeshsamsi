41aab205c45be8173cc2338191453bceac10ff16
Neesh Samsi <avaneeshsamsi@gmail.com>
2023-05-18 16:37:59 +0530

    feat: Add sizing prop for different sizes

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -7,16 +7,28 @@ import YouTube from "./YouTube"
 
 type Props = {
   hoverClr: string
+  size: "sm" | "lg"
 }
 
-const index: FC<Props> = ({ hoverClr }) => {
+const index: FC<Props> = ({ hoverClr, size }) => {
   return (
     <>
       {config.socials.map(({ platform, link }, i) => (
         <Link
           href={link}
           key={i}
-          className={`h-8 w-8 transition-colors md:h-6 md:w-6 lg:h-7 lg:w-7 hover:${hoverClr}`}
+          className={`h-8 w-8 transition-colors ${(() => {
+            switch (size) {
+              case "sm":
+                return "md:h-6 md:w-6 lg:h-7 lg:w-7"
+                break
+              case "lg":
+                return "md:h-7 md:w-7 lg:h-8 lg:w-8"
+                break
+              default:
+                return
+            }
+          })()} hover:${hoverClr}`}
         >
           <li>
             {(() => {
