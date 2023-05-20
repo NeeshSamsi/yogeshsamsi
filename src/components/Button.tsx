02b7534cfaefdfddf306b02403f8bcbe805606d1
Neesh Samsi <avaneeshsamsi@gmail.com>
2023-05-20 16:17:43 +0530

    feat: Button component - Only 1 theme

---
+++ src/components/Button.tsx
@@ -0,0 +1,58 @@
+import type { FC, ReactElement } from "react"
+import Link from "next/link"
+
+type Theme = "Light"
+
+type Props = {
+  as: "button" | "link"
+  text: string
+  type: "Primary" | "Secondary"
+  theme: Theme
+  href?: string
+  icon?: ReactElement
+}
+
+const Button: FC<Props> = ({ as, text, type, theme, icon, href }) => {
+  const commonStyles = `px-3 py-2 sm:px-4 sm:py-1 font-medium text-sm sm:text-lg md:text-base lg:text-lg xl:text-xl transition-all flex items-center gap-3`
+
+  const lightStyles = "text-light transition-colors hover:text-lighter"
+
+  if (as === "button") {
+    return (
+      <button
+        className={
+          type === "Primary"
+            ? `${commonStyles} ${
+                theme === "Light" &&
+                `bg-light text-dark hover:bg-lighter hover:text-darker`
+              }`
+            : `${commonStyles} ${theme === "Light" && lightStyles}`
+        }
+      >
+        <span>{text}</span>
+        <span className="aspect-square w-4 sm:w-5 xl:w-6">{icon}</span>
+      </button>
+    )
+  } else if (as === "link") {
+    return (
+      <Link
+        href={href ? href : "/"}
+        className={
+          type === "Primary"
+            ? `${commonStyles} ${
+                theme === "Light" &&
+                `bg-light text-dark hover:bg-lighter hover:text-darker`
+              }`
+            : `${commonStyles} ${theme === "Light" && lightStyles}`
+        }
+      >
+        <span>{text}</span>
+        <span className="aspect-square w-4 sm:w-5 xl:w-6">{icon}</span>
+      </Link>
+    )
+  } else {
+    return <p>Error - Button</p>
+  }
+}
+
+export default Button
