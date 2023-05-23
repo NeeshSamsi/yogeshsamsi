8d4487524afe58b83bdaac7ae0004850d99feea9
Neesh Samsi <avaneeshsamsi@gmail.com>
2023-05-23 12:41:58 +0530

    feat: Complete about section, change hero text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,14 +12,14 @@ export default async function About() {
   return (
     <>
       <main className="gap-8 px-8 py-12 text-dark md:flex  md:px-0 md:py-0 md:pl-col-inner">
-        <div className="mx-auto flex max-w-[30ch] flex-col items-center justify-center gap-8 text-center md:mx-0 md:max-w-none md:items-start md:gap-6 md:text-start">
-          <h1 className="font-serif text-3xl font-semibold tracking-wide sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
+        <div className="mx-auto flex flex-col items-center justify-center gap-8 text-center md:mx-0 md:items-start md:gap-6 md:text-start">
+          <h1 className="font-serif text-4xl font-bold leading-relaxed tracking-wider sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
             Yogesh Samsi
           </h1>
           <ul className="flex gap-4 md:gap-6">
             <Socials hoverClr="text-darker/80" size="lg" />
           </ul>
-          <div className="space-y-3 text-base sm:text-lg md:text-base lg:text-lg xl:text-xl">
+          <div className="max-w-[30ch] space-y-3 text-base font-medium sm:text-lg md:max-w-none md:text-base lg:text-lg xl:text-xl">
             <p>&ldquo;{quote.text}&rdquo;</p>
             <p>{quote.by}</p>
           </div>
@@ -60,6 +60,28 @@ export default async function About() {
           />
         </div>
       </main>
+
+      <section
+        role="about"
+        className="space-y-12 bg-dark px-8 py-12 text-lighter md:space-y-20 md:px-col-inner md:py-20"
+      >
+        <h2 className="text-center font-serif text-3xl font-semibold tracking-wide sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
+          About Yogesh
+        </h2>
+
+        <div className="md:mx-none mx-auto flex max-w-md flex-col gap-8 text-center text-sm sm:text-base md:max-w-none md:flex-row md:text-start md:text-sm lg:text-base xl:text-lg">
+          <div className="flex flex-col gap-12">
+            {aboutSection.left.map((para, i) => (
+              <p key={i}>{para}</p>
+            ))}
+          </div>
+          <div className="flex flex-col gap-8">
+            {aboutSection.left.map((para, i) => (
+              <p key={i}>{para}</p>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   )
 }
