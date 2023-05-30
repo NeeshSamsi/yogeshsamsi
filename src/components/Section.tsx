f4d8d868426b8ec7223d2707e7d02592588a70a3
Neesh <avaneeshsamsi@gmail.com>
2023-05-30 14:31:17 +0530

    fix: Add larger spacing on larger screens

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -17,7 +17,7 @@ const Section: FC<PropsWithChildren<Props>> = ({
       role={role}
       className={`px-8 py-12 md:px-col-inner md:py-20 2xl:py-32 ${bgClr} ${txtClr}`}
     >
-      <div className="mx-auto max-w-screen-2xl space-y-12 md:space-y-20">
+      <div className="mx-auto max-w-screen-2xl space-y-12 md:space-y-20 2xl:space-y-32">
         {children}
       </div>
     </section>
