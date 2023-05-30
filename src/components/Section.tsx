15451d875991197ff22688511b28a4e4dc0a7c1d
Neesh <avaneeshsamsi@gmail.com>
2023-05-30 13:28:49 +0530

    feat: Abstract away common section styles

---
+++ src/components/Section.tsx
@@ -0,0 +1,27 @@
+import type { FC, PropsWithChildren } from "react"
+
+type Props = {
+  role: string
+  bgClr: string
+  txtClr: string
+}
+
+const Section: FC<PropsWithChildren<Props>> = ({
+  role,
+  bgClr,
+  txtClr,
+  children,
+}) => {
+  return (
+    <section
+      role={role}
+      className={`px-8 py-12 md:px-col-inner md:py-20 2xl:py-32 ${bgClr} ${txtClr}`}
+    >
+      <div className="mx-auto max-w-screen-2xl space-y-12 md:space-y-20">
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export default Section
