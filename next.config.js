deccce52e080868c315b5d8d15434f40175b9060
Neesh <avaneeshsamsi@gmail.com>
2023-06-12 18:48:19 +0530

    feat: Add socials redirect

--- next.config.js
+++ next.config.js
@@ -6,5 +6,24 @@ module.exports = {
   images: {
     domains: ["iili.io"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/facebook",
+        destination: "https://facebook.com/PanditYogeshSamsi",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://instagram.com/yogesh.samsi",
+        permanent: true,
+      },
+      {
+        source: "/youtube",
+        destination: "https://youtube.com/@YogeshSamsiOfficial",
+        permanent: true,
+      },
+    ]
+  },
 }
 // })
