8d14f0b667508bb3a9241d57fb1f450e152dfbda
Neesh <avaneeshsamsi@gmail.com>
2023-06-12 20:06:50 +0530

    feat: types

--- type.d.ts
+++ type.d.ts
@@ -1,1 +1,8 @@
-type Colors = "lighter" | "light" | "dark" | "darker"
+type BackgroundColors = "bg-lighter" | "bg-light" | "bg-dark" | "bg-darker"
+
+type TextColors = "text-lighter" | "text-light" | "text-dark" | "text-darker"
+type BorderColors =
+  | "border-lighter"
+  | "border-light"
+  | "border-dark"
+  | "border-darker"
