ba49263cd76e86ddc68d26310b270b26d5a6709d
Neesh <avaneeshsamsi@gmail.com>
2023-07-30 16:36:28 +0530

    feat: Remove unused utils

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-// import { getPlaiceholder } from "plaiceholder"
-
-// export const getBlurDataURL = async (src: string) => {
-//   const { base64, img } = await getPlaiceholder(src, { size: 10 })
-
-//   return { base64, img }
-// }
