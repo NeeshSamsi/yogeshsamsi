ef17d77c3071e87c70e750955b2d45bb39830908
Neesh <avaneeshsamsi@gmail.com>
2023-08-03 14:18:53 +0530

    feat: Refactor redirects, add /schedule to /#events

--- next.config.js
+++ next.config.js
@@ -1,33 +1,37 @@
-// const { withPlaiceholder } = require("@plaiceholder/next")
+const socialRedirects = [
+  {
+    source: "/facebook",
+    destination: "https://facebook.com/PanditYogeshSamsi",
+    permanent: true,
+  },
+  {
+    source: "/instagram",
+    destination: "https://instagram.com/yogesh.samsi",
+    permanent: true,
+  },
+  {
+    source: "/youtube",
+    destination: "https://youtube.com/@YogeshSamsiOfficial",
+    permanent: true,
+  },
+]
 
 /** @type {import('next').NextConfig} */
 // module.exports = withPlaiceholder({
 module.exports = {
-  images: {
-    domains: ["iili.io"],
-  },
   async redirects() {
     return [
+      {
+        source: "/schedule",
+        destination: "/#events",
+        permanent: true,
+      },
       {
         source: "/masterclass",
         destination: "/learn",
         permanent: true,
       },
-      {
-        source: "/facebook",
-        destination: "https://facebook.com/PanditYogeshSamsi",
-        permanent: true,
-      },
-      {
-        source: "/instagram",
-        destination: "https://instagram.com/yogesh.samsi",
-        permanent: true,
-      },
-      {
-        source: "/youtube",
-        destination: "https://youtube.com/@YogeshSamsiOfficial",
-        permanent: true,
-      },
+      ...socialRedirects,
     ]
   },
 }
