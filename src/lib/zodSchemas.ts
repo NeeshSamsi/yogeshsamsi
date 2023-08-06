4fcd4df568279fdc77813bb5dd03abb27f30afed
Neesh <avaneeshsamsi@gmail.com>
2023-08-06 20:15:13 +0530

    feat: Add contact form schema

--- src/lib/zodSchemas.ts
+++ src/lib/zodSchemas.ts
@@ -5,3 +5,10 @@ export const mailingListSchema = z.object({
   mlEmail: z.string().min(1, "Email is required.").email("Invalid email."),
 })
 export type MailingListSchemaType = z.infer<typeof mailingListSchema>
+
+export const contactFormSchema = z.object({
+  name: z.string().min(1, "Full name is required."),
+  email: z.string().min(1, "Email is required.").email("Invalid email."),
+  message: z.string().min(1, "Message is required."),
+})
+export type ContactFormSchemaType = z.infer<typeof contactFormSchema>
