7956fd9206763f89c6bfd0862cf302528d7d7320
Neesh <avaneeshsamsi@gmail.com>
2023-08-07 18:35:52 +0530

    feat: Remove unused date function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,29 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-type DateStyle = Intl.DateTimeFormatOptions["dateStyle"]
-
-export function formatDates(date: Date[], dateStyle: DateStyle = "medium") {
-  const formatter = new Intl.DateTimeFormat("en", { dateStyle })
-
-  if (date.length === 1) {
-    return formatter.format(date[0])
-  }
-
-  if (date.length > 1) {
-    // for (let i = 0; i < date.length; i++) {
-    //   const prevElement = date[i--]
-    //   const element = date[i]
-    //   let dateString = ""
-    //   if (i === 0) {
-    //     dateString = String(element.getDate())
-    //   }
-    //   if (prevElement.getMonth() === element.getMonth()) {
-    //     dateString = `${dateString}, ${String(element.getDate())}`
-    //   } else if (prevElement.getMonth() !== element.getMonth()) {
-    //     dateString = `${prevElement.getMonth()} `
-    //   }
-    // }
-  }
-}
