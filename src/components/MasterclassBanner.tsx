b6418afa852093a2f1a6bca2966dca2f89e7b501
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-06-21 15:29:54 +0530

    fix: Styling on smallest screens

--- src/components/MasterclassBanner.tsx
+++ src/components/MasterclassBanner.tsx
@@ -14,11 +14,11 @@ export default function MasterclassBanner({ title }: Props) {
   if (pathname === "/masterclass") return null
 
   return (
-    <p className="flex items-center justify-center bg-lighter py-1 text-center text-base font-medium text-darker sm:text-lg lg:py-3 lg:text-xl 2xl:text-2xl">
+    <p className="flex flex-col items-center justify-center bg-lighter px-8 py-1 text-center text-base font-medium text-darker sm:flex-row sm:text-lg lg:py-3 lg:text-xl 2xl:text-2xl">
       <span>{title} by Yogesh Samsi</span>
       <Button as="link" theme="Darker" type="Secondary">
         <span className="font-semibold underline">Join now</span>
-        <ArrowRightIcon className="aspect-square size-6 stroke-[2.5px]" />
+        <ArrowRightIcon className="aspect-square size-4 stroke-[2.5px] lg:size-6" />
       </Button>
     </p>
   )
