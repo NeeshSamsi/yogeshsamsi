8e59a1e27423723bb5b022173b7f61fe08166b43
Neesh <avaneeshsamsi@gmail.com>
2025-06-14 17:57:11 +0200

    fix: Link in masterclass banner

--- src/components/MasterclassBanner.tsx
+++ src/components/MasterclassBanner.tsx
@@ -16,7 +16,7 @@ export default function MasterclassBanner({ title }: Props) {
   return (
     <p className="flex flex-col items-center justify-center bg-lighter px-8 py-1 text-center text-lg font-medium text-darker sm:flex-row sm:text-xl">
       <span>{title} by Yogesh Samsi</span>
-      <Button as="link" theme="Darker" type="Secondary">
+      <Button as="link" theme="Darker" type="Secondary" href="/masterclass">
         <span className="font-semibold underline">Join now</span>
         <ArrowRightIcon className="aspect-square size-4 stroke-[2.5px] lg:size-6" />
       </Button>
