164f7c6673e022e53e275720729e336a3444939e
Neesh <avaneeshsamsi@gmail.com>
2025-07-05 22:33:02 +0200

    feat: Masterclass registration action

---
+++ src/app/actions/masterclass.ts
@@ -0,0 +1,47 @@
+"use server"
+
+import bento from "@/lib/bento"
+import reader from "@/lib/keystatic"
+import { actionClient } from "@/lib/safeAction"
+import { masterclassFormSchema } from "@/lib/zodSchemas"
+import { redirect } from "next/navigation"
+
+export const registerMasterclass = actionClient
+  .inputSchema(masterclassFormSchema)
+  .action(
+    async ({ parsedInput: { name, email } }): Promise<{ success: boolean }> => {
+      const first_name = name.split(" ")[0]
+      const last_name = name.split(" ").slice(1).join(" ")
+
+      const masterclass = await reader.singletons.masterclass.read()
+      if (!masterclass)
+        throw new Error("Keystatic Content Not Found - Masterclass Page")
+      const { formLink } = masterclass
+      if (!formLink) {
+        throw new Error("Keystatic Content Not Found - Masterclass Form Link")
+      }
+
+      try {
+        const user = await bento.V1.Subscribers.getSubscribers({ email })
+        const registered = await bento.V1.track({
+          email,
+          type: "$masterclass.register",
+          fields: {
+            first_name,
+            last_name,
+          },
+        })
+        if (!registered) {
+          return { success: false }
+        }
+        if (!user || !user.attributes.fields?.batch) {
+          redirect(formLink)
+        } else {
+          return { success: true }
+        }
+      } catch (err) {
+        console.error(err)
+        return { success: false }
+      }
+    },
+  )
