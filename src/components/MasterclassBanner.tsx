14294d66ffeea7d8e15259e5cc801a596169ad24
Neesh <avaneeshsamsi@gmail.com>
2025-07-05 22:33:14 +0200

    feat: Also switching to shadcn button

--- src/components/MasterclassBanner.tsx
+++ src/components/MasterclassBanner.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { usePathname } from "next/navigation"
-import Button from "./Button"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 import { ArrowRightIcon } from "@heroicons/react/24/outline"
 
 type Props = {
@@ -16,9 +17,11 @@ export default function MasterclassBanner({ title }: Props) {
   return (
     <p className="flex flex-col items-center justify-center bg-lighter px-8 py-1 text-center text-lg font-medium text-darker sm:flex-row sm:text-xl">
       <span>{title} by Yogesh Samsi</span>
-      <Button as="link" theme="Darker" type="Secondary" href="/masterclass">
-        <span className="font-semibold underline">Join now</span>
-        <ArrowRightIcon className="aspect-square size-4 stroke-[2.5px] lg:size-6" />
+      <Button asChild variant="secondary" theme="darker">
+        <Link href="/masterclass">
+          <span className="font-semibold underline">Join now</span>
+          <ArrowRightIcon className="aspect-square size-4 stroke-[2.5px] lg:size-6" />
+        </Link>
       </Button>
     </p>
   )
