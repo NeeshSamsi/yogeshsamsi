f933a316fc53310a651c4436fa6b9e630f935191
Neesh <avaneeshsamsi@gmail.com>
2025-07-18 12:12:28 +0200

    fix: Handle existing subscribers

--- src/app/actions/mailing-list.ts
+++ src/app/actions/mailing-list.ts
@@ -7,22 +7,32 @@ import bento from "@/lib/bento"
 export const subscribe = actionClient
   .inputSchema(mailingListSchema)
   .action(async ({ parsedInput: { mlName: name, mlEmail: email } }) => {
-    const first_name = name.split(" ")[0]
-    const last_name = name.split(" ").slice(1).join(" ")
-
     try {
-      await bento.V1.track({
-        email,
-        type: "$opt.in",
-        fields: {
-          first_name,
-          last_name,
-        },
-      })
+      const user = await bento.V1.Subscribers.getSubscribers({ email })
 
-      return {
-        success: true,
-        message: "Thank you for joining the Mailing List!",
+      if (!user) {
+        const first_name = name.split(" ")[0]
+        const last_name = name.split(" ").slice(1).join(" ")
+
+        await bento.V1.track({
+          email,
+          type: "$opt.in",
+          fields: {
+            first_name,
+            last_name,
+          },
+        })
+
+        return {
+          success: true,
+          message:
+            "Please check your inbox and confirm your subsription to Yogesh Samsi Updates.",
+        }
+      } else {
+        return {
+          success: true,
+          message: "You are already subscribed.",
+        }
       }
     } catch (err) {
       console.error(err)
