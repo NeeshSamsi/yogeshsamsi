fd70224de0fafcf320ae29151d4deea6aa9c05f8
Neesh <avaneeshsamsi@gmail.com>
2026-06-09 13:32:30 +0700

    feat: fix hmr issues

--- next.config.js
+++ next.config.js
@@ -22,7 +22,8 @@ const socialRedirects = [
 ]
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
   async redirects() {
     return [
       {
@@ -49,54 +50,28 @@ module.exports = {
       ...socialRedirects,
     ]
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/public/files/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-  //         {
-  //           key: "Access-Control-Allow-Methods",
-  //           value: "GET",
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
 }
 
-// Injected content via Sentry wizard below
+// Sentry's withSentryConfig injects a webpack() function that conflicts with
+// Turbopack's module registry and causes persistent HMR failures in dev.
+// Runtime error capturing still works via instrumentation.ts in all modes.
+// The webpack plugin (source map uploads) only runs for production builds.
+if (process.env.NODE_ENV === "production") {
+  const { withSentryConfig } = require("@sentry/nextjs")
+  module.exports = withSentryConfig(nextConfig, {
+    org: "yogesh-samsi",
+    project: "website",
 
-const { withSentryConfig } = require("@sentry/nextjs")
+    silent: !process.env.CI,
 
-module.exports = withSentryConfig(module.exports, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+    widenClientFileUpload: true,
 
-  org: "yogesh-samsi",
-  project: "website",
+    tunnelRoute: "/monitoring",
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+    disableLogger: true,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-})
+    automaticVercelMonitors: true,
+  })
+} else {
+  module.exports = nextConfig
+}
